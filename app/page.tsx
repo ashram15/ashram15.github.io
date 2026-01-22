989c075f9cc6983fc24a59ad559390e3270c610c
Aishwarya Ramesh <ashram1015@gmail.com>
2026-01-22 14:05:42 -0600

    Refactor: migratifrom React to Next.js

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import App from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-slate-900">
+      <App />
+    </main>
+  );
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'shrikhand': ['Shrikhand', 'serif'],
+            },
+        },
+    },
+    plugins: [],
+}
